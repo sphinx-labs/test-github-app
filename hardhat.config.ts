ac9eef2b65480785a1b728256d98e8edbc0eea35
Ryan Pate <ryantpate97@gmail.com>
2023-02-19 11:32:43 -0800

    trigger

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@chugsplash/plugins'
 
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -16,7 +18,39 @@ const config: HardhatUserConfig = {
         },
       },
     ]
-  }
+  },
+  networks: {
+    goerli: {
+      chainId: 5,
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+    },
+    "optimism-goerli": {
+      chainId: 420,
+      url: `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+    },
+    optimism: {
+      chainId: 10,
+      url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+    },
+    arbitrum: {
+      chainId: 42161,
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts,
+    },
+    "arbitrum-goerli": {
+      chainId: 421613,
+      url: `https://arb-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+    },
+  },
 };
 
 export default config;
