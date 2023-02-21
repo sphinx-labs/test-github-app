157c47431f2a7bcb3ae03e4cd12cf4b295c38d1d
Ryan Pate <ryantpate97@gmail.com>
2023-02-21 12:54:00 -0800

    a

--- chugsplash/hello-chugsplash.ts
+++ chugsplash/hello-chugsplash.ts
@@ -14,6 +14,15 @@ const config: UserChugSplashConfig = {
         otherStorage: '0x1111111111111111111111111111111111111111',
       },
     },
+    MySecondContract: {
+      contract: 'HelloChugSplash',
+      variables: {
+        number: 1,
+        stored: false,
+        storageName: 'First',
+        otherStorage: '0x1111111111111111111111111111111111111111',
+      },
+    },
   },
 }
 
