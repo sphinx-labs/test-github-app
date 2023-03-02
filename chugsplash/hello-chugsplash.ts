544fc980436899ae41a30fe50986fcd0d1bb4d7b
Ryan Pate <ryantpate97@gmail.com>
2023-03-01 21:32:48 -0700

    trigger

--- chugsplash/hello-chugsplash.ts
+++ chugsplash/hello-chugsplash.ts
@@ -2,13 +2,13 @@ import { UserChugSplashConfig } from '@chugsplash/core'
 
 const config: UserChugSplashConfig = {
   options: {
-    projectName: 'Hello ChugSplash Pate',
+    projectName: 'Hello ChugSplash RP',
   },
   contracts: {
     MyFirstContract: {
       contract: 'HelloChugSplash',
       variables: {
-        number: 11,
+        number: 1,
         stored: false,
         storageName: 'false',
         otherStorage: '0x1111111111111111111111111111111111111111',
