ff96c7de1d72e87e3e668cb6f3b7b6d152107c1a
Ryan Pate <ryantpate97@gmail.com>
2023-03-01 20:29:01 -0700

    trigger

--- chugsplash/hello-chugsplash.ts
+++ chugsplash/hello-chugsplash.ts
@@ -8,7 +8,7 @@ const config: UserChugSplashConfig = {
     MyFirstContract: {
       contract: 'HelloChugSplash',
       variables: {
-        number: 4,
+        number: 5,
         stored: false,
         storageName: 'false',
         otherStorage: '0x1111111111111111111111111111111111111111',
