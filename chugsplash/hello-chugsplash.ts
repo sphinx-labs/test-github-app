25f44719d5cc41f2f8230b739e416c6c9c84e277
Ryan Pate <ryantpate97@gmail.com>
2023-05-17 16:59:27 -0700

    trigger deployment

--- chugsplash/hello-chugsplash.ts
+++ chugsplash/hello-chugsplash.ts
@@ -2,53 +2,14 @@ import { UserChugSplashConfig } from '@chugsplash/core'
 
 const config: UserChugSplashConfig = {
   options: {
-    projectName: 'Ryan Test',
+    organizationID: '0x5a72d86ce90b1bc7afdd472ec7bb0182431949e55a0b8185252d276c67cd7c46',
+    projectName: 'Hello ChugSplash',
   },
   contracts: {
     MyFirstContract: {
       contract: 'HelloChugSplash',
       variables: {
-        number: 3,
-        stored: false,
-        storageName: 'false',
-        otherStorage: '0x1111111111111111111111111111111111111111',
-        testInt: 1
-      },
-    },
-    MySecondContract: {
-      contract: 'HelloChugSplash',
-      variables: {
-        number: 3,
-        stored: false,
-        storageName: 'false',
-        otherStorage: '0x1111111111111111111111111111111111111111',
-        testInt: 1
-      },
-    },
-    MyThirdContract: {
-      contract: 'HelloChugSplash',
-      variables: {
-        number: 3,
-        stored: false,
-        storageName: 'false',
-        otherStorage: '0x1111111111111111111111111111111111111111',
-        testInt: 1
-      },
-    },
-    MyForthContract: {
-      contract: 'HelloChugSplash',
-      variables: {
-        number: 3,
-        stored: false,
-        storageName: 'false',
-        otherStorage: '0x1111111111111111111111111111111111111111',
-        testInt: 1
-      },
-    },
-    MyFiveContract: {
-      contract: 'HelloChugSplash',
-      variables: {
-        number: 3,
+        number: 1,
         stored: false,
         storageName: 'false',
         otherStorage: '0x1111111111111111111111111111111111111111',
